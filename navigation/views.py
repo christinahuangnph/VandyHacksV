6bd5cededabccb61fa51db21ede99af6e56c5730
emilymarkert <emily.markert@vanderbilt.edu>
2018-11-03 15:04:50 -0500

    fixing token problem

--- navigation/views.py
+++ navigation/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 from .utils import prompts
 
-
+@ensure_csrf_cookie
 def index(request):
     context = {}
     if(request.method == 'POST'): # form submission
