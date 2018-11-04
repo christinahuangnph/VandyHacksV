908a605d9ea61a2926bd718ec6758d3925919966
emilymarkert <emily.markert@vanderbilt.edu>
2018-11-03 20:24:39 -0500

    linking breathe animation

--- navigation/views.py
+++ navigation/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.http import JsonResponse
 
 from .utils import prompts
 
 @ensure_csrf_cookie
 def index(request):
-    context = {}
+
     if(request.method == 'POST'): # form submission
-        context['prompt'] = prompts.prompt_user()
-        context['exercise'] = prompts.launch_exercise()
+        return JsonResponse({'prompt':prompts.prompt_user(),
+                             'exercise': prompts.launch_exercise()})
 
-
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html')
