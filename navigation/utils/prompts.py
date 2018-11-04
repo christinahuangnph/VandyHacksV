78fae01a8815dd96c051682a15e03036df8a9929
emilymarkert <emily.markert@vanderbilt.edu>
2018-11-03 23:02:41 -0500

    prompts

--- navigation/utils/prompts.py
+++ navigation/utils/prompts.py
@@ -3,20 +3,19 @@ import random
 import speech_recognition as sr
 from fuzzysearch import find_near_matches
 
-AUDIO_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
-                          'navigation/static/navigation/audiofiles')
+AUDIO_PATH = '/static/navigation/audiofiles'
 
-commands = ['sensory_counting', 'mantra', 'object', 'breathing']
+commands = ['mantra', 'object', 'breathing']
 
 
 def prompt_user():
-    return os.path.join(AUDIO_PATH, 'prompt.mp3')
+    return AUDIO_PATH + '/prompt.mp3'
 
 def launch_exercise():
     user_in = accept_audio()
     command = process_speech(user_in)
     path = exercise_path(command)
-    return path
+    return (path, command)
 
 
 # reads audio from mic
@@ -47,19 +46,17 @@ def speech_to_text(audio_input):
 
 
 def process_speech(transcript):
-    if find_near_matches('count', transcript, max_l_dist=2) or find_near_matches('sens', transcript, max_l_dist=2):
-        return 'sensory_count'
-    if find_near_matches('mantra', transcript, max_l_dist=2) or find_near_matches('make', transcript, max_l_dist=2):
+    if find_near_matches('mantra', transcript, max_l_dist=2):
         return 'mantra'
     if find_near_matches('breath', transcript, max_l_dist=2):
         return 'breathing'
-    if find_near_matches('focus', transcript, max_l_dist=2) or find_near_matches('object', transcript, max_l_dist=2):
+    if find_near_matches('focus', transcript, max_l_dist=2):
         return 'object'
-    return ''
+    # pick random command
+    index = random.randint(0, len(commands) - 1)
+    return commands[index]
 
 
 def exercise_path(command):
-    if command == '': # random exercise
-        index = random.randint(0, len(commands)-1)
-        command = commands[index]
-    return os.path.join(AUDIO_PATH, command +'.mp3')
+    return AUDIO_PATH + '/' + command + '.mp3'
+
